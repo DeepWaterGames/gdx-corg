9d3a694b668da9354139ce50f17782c429565f06
Kylestach1678 <Kylestach99@gmail.com>
2015-09-06 22:42:38 -0700

    Initial commit

---
+++ android/src/net/deepwater/corgi/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package net.deepwater.corgi.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import net.deepwater.corgi.CorgiGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new CorgiGame(), config);
+	}
+}
---
+++ html/src/net/deepwater/corgi/client/HtmlLauncher.java
@@ -0,0 +1,19 @@
+package net.deepwater.corgi.client;
+
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.gwt.GwtApplication;
+import com.badlogic.gdx.backends.gwt.GwtApplicationConfiguration;
+import net.deepwater.corgi.CorgiGame;
+
+public class HtmlLauncher extends GwtApplication {
+
+        @Override
+        public GwtApplicationConfiguration getConfig () {
+                return new GwtApplicationConfiguration(480, 320);
+        }
+
+        @Override
+        public ApplicationListener getApplicationListener () {
+                return new CorgiGame();
+        }
+}
